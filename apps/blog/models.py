a55c5df0a3a326a4608490f4d3275d71201d4efd
Dylan Buchi <devybuchi@gmail.com>
2021-02-13 01:42:10 -0300

    create blog model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    body_text = models.TextField(name='text')
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        title_text = self.title
+
+        return title_text if len(title_text) < 25 \
+            else f"{title_text[:25]}..."
+
+
+class BlogPost(Post):
+    ...
